35554a5e669825bd1c1c8b530d9ad7c802c8b29a
syed sufiyan ali <sufiyantdc@gmail.com>
2026-06-07 00:19:47 +0530

    day-2

---
+++ day-2/assignment.js
@@ -0,0 +1,32 @@
+let userName = "sarah";
+let hour = 9;
+let greeting = `good morning, ${userName}!`;
+
+console.log(greeting);
+
+let userAge = 19;
+let minimumAge = 18;
+let isOldEnough = userAge >= minimumAge;
+
+console.log(isOldEnough);
+
+let email = "sufiyantdc@gmail.com";
+let firstChar = email[0];
+let isValid = firstChar >= "a" && firstChar <= "z";
+
+console.log(isValid);
+
+let productName = "wireless mouse";
+let productPrice = "30";
+let isInStock = true;
+
+let description = `product: ${productName} | price ${productPrice} | instock: ${isInStock}`;
+console.log(description);
+
+let firstName = "John";
+let middleName = "Robert";
+let lastName = "Smith";
+
+let initials = firstName[0] + middleName[0] + lastName[0];
+
+console.log(initials);
---
+++ day-2/script.js
@@ -0,0 +1,14 @@
+let items = "apple";
+let price = 100;
+
+console.log("the cost of" + items + "is" + price);
+
+console.log(`the cost of ${items} is ${price} dollars.`);
+
+let city = "london";
+console.log(city[0]);
+console.log(city[1]);
+console.log(city[2]);
+console.log(city[3]);
+console.log(city[4]);
+console.log(city[5]);
