6c2c22daba2f0c0c0855e18b674e764f341fe332
syed sufiyan ali <sufiyantdc@gmail.com>
2026-06-13 00:01:30 +0530

    day-5 done

---
+++ day-5/assignment.js
@@ -0,0 +1,32 @@
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (let j = 2; j <= 20; j++) {
+//   console.log(j);
+// }
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+// console.log("Blast off!");
+
+// let num = 7;
+
+// for (let i = 1; i <= 10; i++) {
+//   // let product = num * i;
+//   console.log(`${num}x${i}=${num * i}`);
+// }
+
+// for (let i = 1; i <= 4; i++) {
+//   console.log("--", i);
+//   for (let j = 1; j <= 4; j++) {
+//     console.log(j);
+//   }
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= i; i++) {
+//     console.log();
+//   }
+// }
---
+++ day-5/script.js
@@ -0,0 +1,22 @@
+// let favoriteMovie = "dangle";
+// let guess = "";
+
+// while (guess !== favoriteMovie && guess !== "quit") {
+//   guess = prompt("guess  my favorite movie: ");
+
+//   if (guess === favoriteMovie) {
+//     console.log("congratulation 🎉");
+//   } else if (guess === "quit") {
+//     console.log(`better luck next time ${favoriteMovie0}`);
+//   } else {
+//     console.log("try again");
+//   }
+// }
+
+// let sum = 0;
+// let num = 1;
+// while (sum < 50) {
+//   sum += num;
+//   console.log(sum);
+//   num++;
+// }
