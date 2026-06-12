5659db16b1f635a11d37f3ae447473b7919e1dd1
syed sufiyan ali <sufiyantdc@gmail.com>
2026-06-13 00:12:22 +0530

    day-5 finished

--- day-5/assignment.js
+++ day-5/assignment.js
@@ -30,3 +30,10 @@
 //     console.log();
 //   }
 // }
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("fist number divisibele by 3 and 5 is : ", i);
+    break;
+  }
+}
